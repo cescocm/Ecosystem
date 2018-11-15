89023fb312f9a3e9bf3a2c6ebf8bbe9a11c7d484
Salvador <salvaom11@gmail.com>
2018-11-15 11:54:43 +0100

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python
 import os
-import re
+import imp
 from setuptools import setup, find_packages
 
 
-ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
-init_path = os.path.join(ROOT_PATH, 'source', 'ecosystem', '__init__.py')
+version_file = os.path.abspath('source/ecosystem/__init__.py')
+version_mod = imp.load_source('ecosystem', version_file)
 
-
-with open(init_path) as f:
-    contents = f.read()
-    VERSION = re.match(r'.*__version__ = \'(.*?)\'', contents, re.DOTALL)
-    VERSION = VERSION.group(1)
 
 setup(
-    name='Ecosystem',
-    version=VERSION,
-    description='Cross-platform environment management system',
-    url='https://github.com/PeregrineLabs/Ecosystem',
-    author='Peregrine Labs',
-    author_email='contact@peregrinelabs.com',
+    name='ecosystemS',
+    version=version_mod.__version__,
     packages=find_packages(os.path.join(os.path.dirname(__file__), 'source')),
     package_dir={
         '': 'source'
